471f1c516e5f3df56ad927a69d6c96f0a602a74f
Melal <sfa009361@gmail.com>
2026-07-27 14:23:33 -0700

    Fix lock file

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
